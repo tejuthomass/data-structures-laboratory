82b7d81df1a1f0a37f1f6c4aefff99757fd7b92f
Teju Thomas <thomasteju7@gmail.com>
2022-09-22 17:54:46 +0530

    Completed stack using array and linked list

---
+++ stack-using-array.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#define SIZE 5
+
+void push();
+void pop();
+void display();
+
+int s[SIZE], top = -1, i, item;
+
+void main()
+{
+    int ch;
+    
+    clrscr();
+    printf("$$$ STACK IMPLEMENTATION USING ARRAY $$$\n");
+    
+    while (1)
+    {
+        printf("\nSelect any operation:\n1 - PUSH\n2 - POP\n3 - DISPLAY\n4 - EXIT\n\nYour choice: ");
+        scanf("%d", &ch);
+        
+        switch(ch)
+        {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+        default:
+            printf("Invalid Choice!!");
+        }
+    }
+}
+
+void push()
+{
+    if(top == SIZE - 1)
+    {
+        printf("Stack overflow!!\n");
+    }
+    else
+    {
+        top++;
+        printf("Enter the element: ");
+        scanf("%d", &s[top]);
+        printf("%d has been successfully pushed into stack!\n", s[top]);
+    }
+}
+
+void pop()
+{
+    if(top == -1)
+    {
+        printf("Stack underflow!!\n");
+    }
+    else
+    {
+        printf("%d has been successfully popped out of stack!\n", s[top]);
+        top--;
+    }
+}
+
+void display()
+{
+    if(top == -1)
+    {
+        printf("Stack underflow!!\n");
+    }
+    else
+    {
+        printf("Your stack:\n");
+        for(i = top; i >= 0; i--)
+        {
+            printf("%d\n", s[i]);
+        }
+    }
+}
---
+++ stack-using-linked-list.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+typedef struct stack
+{
+    int data;
+    struct stack *next;
+} node;
+
+node *newn, *top = NULL, *temp;
+
+void push();
+void pop();
+void display();
+
+void main()
+{
+    int ch;
+    
+    clrscr();
+    printf("$$$ STACK IMPLEMENTATION USING LINKED LIST $$$\n");
+    
+    while (1)
+    {
+        printf("\nSelect any operation:\n1 - PUSH\n2 - POP\n3 - DISPLAY\n4 - EXIT\n\nYour choice: ");
+        scanf("%d", &ch);
+        
+        switch (ch)
+        {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+        default:
+            printf("Invalid Choice!!");
+        }
+    }
+}
+
+void push()
+{
+    newn = (node *)malloc(sizeof(node));
+
+    printf("Enter the element: ");
+    scanf("%d", &newn->data);
+
+    if (top == NULL)
+    {
+        newn->next = NULL;
+    }
+    else
+    {
+        newn->next = top;
+    }
+    top = newn;
+    printf("%d has been successfully pushed into stack!\n", newn->data);
+}
+
+void pop()
+{
+    if(top == NULL)
+    {
+        printf("Stack underflow!!\n");
+    }
+    else
+    {
+        printf("%d has been successfully popped out of stack!\n", top->data);
+        temp = top;
+        top = top->next;
+    }
+    free(temp);
+}
+
+void display()
+{
+    if(top == NULL)
+    {
+        printf("Stack underflow!!\n");
+    }
+    else
+    {
+        
+        printf("Your stack:\n");
+        for (temp = top; temp != NULL; temp = temp->next)
+        {
+            printf("%d\n", temp->data);
+        }
+    }
+}
